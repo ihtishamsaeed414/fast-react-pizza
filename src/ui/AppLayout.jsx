c43d8875b00d2a1190e701d4ff87d5c0b0221226
Ihtisham Saeed <111792380+ihtishamsaeed414@users.noreply.github.com>
2024-09-01 01:30:30 +0500

    tailwind css added

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,18 +1,21 @@
-import Header from "./Header";
-import Loader from "./Loader";
-import CartOverview from "../features/cart/CartOverview";
-import { Outlet, useNavigation } from "react-router-dom";
+import Header from './Header';
+import Loader from './Loader';
+import CartOverview from '../features/cart/CartOverview';
+import { Outlet, useNavigation } from 'react-router-dom';
 function AppLayout() {
   const navigation = useNavigation();
-  const isLoading = navigation.state === "loading";
+  const isLoading = navigation.state === 'loading';
   return (
-    <div className="layout">
+    <div className="grid h-screen grid-rows-[auto-1fr-auto]">
       {isLoading && <Loader />}
-      <Header />
-      <main>
-        <Outlet />
-      </main>
 
+      <Header />
+
+      <div className="overflow-scroll">
+        <main className="mx-auto max-w-3xl">
+          <Outlet />
+        </main>
+      </div>
       <CartOverview />
     </div>
   );
